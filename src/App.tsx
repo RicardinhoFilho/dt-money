5ecf88946956bfa6dc1010f95dbaee818a86e187
Ricardo <ricardinhogiasson16@gmail.com>
2021-05-11 16:09:45 -0300

    Add Transactios Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './Components/Dashboard';
 import { Header } from './Components/Header';
 import { TransactionsTable } from './Components/TransacionTable';
 import { GlobalStyle } from './styles/global';
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+  
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+
+
   return (
     <>
-    <Header/>
+    <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
     <Dashboard/>
-    <TransactionsTable/>
+
+    <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+      <h2>Cadastrar Transação</h2>
+    </Modal>
      <GlobalStyle/>
     </>
   );
--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -1,9 +1,12 @@
 import{Container} from "./styles"
 import {Summary} from "../Summary"
+import React from "react";
+import { TransactionsTable } from "../TransacionTable";
 export function Dashboard(){
     return (
         <Container>
             <Summary/>
+            <TransactionsTable/>
         </Container>
     );
 }
