669f982c81aa63e7a07fbd428e9fae3a835b8051
Ricardo <ricardinhogiasson16@gmail.com>
2021-05-11 14:17:20 -0300

    First Commit, GlobalStyle, Header Component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from './Components/Header';
+import { GlobalStyle } from './styles/global';
+export function App() {
+  return (
+    <>
+    <Header/>
+     <GlobalStyle/>
+    </>
+  );
+}
+
+
